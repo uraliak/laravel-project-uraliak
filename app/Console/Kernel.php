1c8e18ed1b96c2e8a804120bbf70ce09da58ed8f
uraliak <uraliak@mail.ru>
2023-09-14 23:16:27 +0300

    create database

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -10,7 +10,7 @@ class Kernel extends ConsoleKernel
     /**
      * Define the application's command schedule.
      */
-    protected function schedule(Schedule $schedule): void
+    protected function schedule(Schedule $schedule): void //планировщик задач
     {
         // $schedule->command('inspire')->hourly();
     }
